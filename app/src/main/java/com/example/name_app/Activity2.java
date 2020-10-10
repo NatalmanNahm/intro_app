fe237083c0951f298d5aeae5380ef1db2faf1eaf
Natalman <natalnahm@gmail.com>
2020-10-10 14:30:14 -0500

    Did the logic on activity2 a message is displayed when username and password are wrong or right.

--- app/src/main/java/com/example/name_app/Activity2.java
+++ app/src/main/java/com/example/name_app/Activity2.java
@@ -4,16 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
+
+    //initialize
+    public String mUsername = "Natalman";
+    public String mPassword = "Junior228";
+    public TextView mWelcomeTextView;
+    public String mMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
+        //Reference the textView to its appropriate fiels\d
+        mWelcomeTextView = (TextView) findViewById(R.id.welcome_textView);
+
+        //Get value sent to the activity2
         Intent intent = getIntent();
-        String str = intent.getStringExtra("msg");
+        String sentUserName = intent.getStringExtra("USERNAME");
+        String sentPassword = intent.getStringExtra("PASSWORD");
+
+        //Check the condition to see if the username and password are right
+        //Then display and welcome message on the screen
+        if (mUsername.equals(sentUserName) && mPassword.equals(sentPassword)){
+            mMessage = "Welcome " + sentUserName;
+        } else {
+            mMessage = "Username and Password invalid please try again";
+        }
+        mWelcomeTextView.setText(mMessage);
 
     }
 }
