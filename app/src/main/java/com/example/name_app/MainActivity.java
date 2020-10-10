5a50c4d4fa885388b7589b6c0ca491c9ee0d7798
Natalman <natalnahm@gmail.com>
2020-10-10 14:13:01 -0500

    Added some logic to the code so we can get values of edittexts and send them over to the activity2.

--- app/src/main/java/com/example/name_app/MainActivity.java
+++ app/src/main/java/com/example/name_app/MainActivity.java
@@ -8,20 +8,39 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Initialize
+    public EditText mUserEdtText;
+    public EditText mPasswordEditText;
+    public Button mSubmitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Reference EditText and button to their appropriate field
+        mUserEdtText = (EditText) findViewById(R.id.username_EditText);
+        mPasswordEditText = (EditText) findViewById(R.id.password_editText);
+        mSubmitBtn = (Button) findViewById(R.id.submit_btn);
     }
 
     public void goToActivity(View view){
         Log.i("INFO", "Transitioning to " + Activity2.class.getName());
+
+        //Get value of the EditText and format them to string
+        String username = mUserEdtText.getText().toString();
+        String password = mPasswordEditText.getText().toString();
+
+        //Start activity2 and send some data over
         Intent intent = new Intent(getApplicationContext(), Activity2.class);
-        intent.putExtra("msg", "Hello activity two");
+        intent.putExtra("USERNAME", username);
+        intent.putExtra("PASSWORD", password);
         startActivity(intent);
     }
 }
