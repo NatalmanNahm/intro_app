7410c0bfa9d06ee8332808746d875f541170060b
Natalman <natalnahm@gmail.com>
2020-10-10 14:45:53 -0500

    Just a little trick to the app.

--- app/src/main/java/com/example/name_app/Activity2.java
+++ app/src/main/java/com/example/name_app/Activity2.java
@@ -3,6 +3,7 @@ package com.example.name_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -19,7 +20,7 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        //Reference the textView to its appropriate fiels\d
+        //Reference the textView to its appropriate field
         mWelcomeTextView = (TextView) findViewById(R.id.welcome_textView);
 
         //Get value sent to the activity2
@@ -32,6 +33,7 @@ public class Activity2 extends AppCompatActivity {
         if (mUsername.equals(sentUserName) && mPassword.equals(sentPassword)){
             mMessage = "Welcome " + sentUserName;
         } else {
+            mWelcomeTextView.setTextColor(Color.parseColor("#C62828"));
             mMessage = "Username and Password invalid please try again";
         }
         mWelcomeTextView.setText(mMessage);
